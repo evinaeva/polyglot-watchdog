b04ed1bdc6521298a3750c2c1c1d851fe9edf057
evinaeva <11227942+evinaeva@users.noreply.github.com>
2026-03-04 17:44:02 +0200

    Create init.py

---
+++ pipeline/init.py
@@ -0,0 +1,6 @@
+"""Pipeline package for the localization QA pipeline.
+
+This package contains the entry point for executing the end‑to‑end
+localization QA pipeline. Additional helper modules could be added
+here in future iterations.
+"""
